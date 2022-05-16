3d695cbdd66d6efdda87830b9a4e2e763865ab33
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-16 21:22:18 +0100

    bruh

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    l = 0
+    bruh = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            l += 1
+            bruh += 1
         except (TypeError, ValueError):
             pass
     print()
-    return l
+    return bruh
