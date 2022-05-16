2712582690b8061f20084ca76b3390751cfa34de
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-16 21:09:26 +0100

    diff meth

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     l = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            l += 1
-        except (TypeError, ValueError):
-            pass
-    print("")
-    return l
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                l += 1
+    except (TypeError, ValueError):
+        return
+    else:
+        print()
+        return l
