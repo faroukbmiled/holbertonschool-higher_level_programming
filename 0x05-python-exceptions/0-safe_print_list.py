e3eb1ad26b9e35dc3db50654147583ebda38eb7c
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-16 20:15:19 +0100

    format?

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     l = 0
     for i in range(0, x):
         try:
-            print(f'{my_list[i]}', end='')
+            print("{:d}".format(my_list[i]), end="")
             l += 1
         except:
             pass
