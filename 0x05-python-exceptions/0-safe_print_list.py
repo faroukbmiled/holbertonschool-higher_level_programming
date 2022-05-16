a77f65b6ba526bb21f5038042f79891c268728dd
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-16 20:08:10 +0100

    diff meth

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        l = 0
-        for index in range(0, x):
-            print(f'{my_list[index]}', end='')
+    l = 0
+    for i in range(0, x):
+        try:
+            print(f'{my_list[i]}', end='')
             l += 1
-        print()
-        return x
-    except IndexError:
-        print()
-        return index
+        except:
+            pass
+    print('')
+    return l
