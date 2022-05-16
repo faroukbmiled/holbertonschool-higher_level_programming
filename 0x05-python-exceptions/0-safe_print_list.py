31c8dad302af48990ff4de7f64224422a97e734a
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-16 20:23:15 +0100

    bruh

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
             l += 1
         print()
         return x
-    except IndexError:
+    except IndexError as IE:
         print()
         return index
-    return index
