15b971f9d29aa09e88f8b4a461baef0529b6e6c0
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-30 23:05:55 +0100

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""returns True if the object is an instance of,
+or if the object is an instance of a class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of,
+or if the object is an instance of a class
+"""
+
+    return isinstance(obj, a_class)
