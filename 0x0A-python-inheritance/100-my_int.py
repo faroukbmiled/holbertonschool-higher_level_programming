08bdea5cb977bb2eaaf078c786140757a1e540a5
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-30 23:56:24 +0100

    up

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,7 @@ class MyInt(int):
     """ class MyInt that inherits from int"""
 
     def __eq__(self, other):
-        return int(self) != int(other)
+        return int(self) is not int(other)
 
     def __ne__(self, other):
         return int(self) == int(other)
