b88657f891f584a07aad1e2f2d180f6765989e6e
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-31 21:02:35 +0100

    req

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""reads a text file (UTF8) and prints it to stdout"""
+"""module for read_file."""
 
 
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding='utf-8') as f:
         read = f.read()
-        print(read)
+        print(read, end="")
