33832e194d2a2ce7c3dde213511ec5821c997cf8
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-31 21:01:45 +0100

    initial

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding='utf-8') as f:
+        read = f.read()
+        print(read)
