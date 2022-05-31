8af02b106ff8086ae40933acc51edeb78512f9bf
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-31 23:12:52 +0100

    fixes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,15 +5,21 @@ and then save them to a file
 """
 
 
-import sys
+from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+file = "add_item.json"
+
 try:
-    list = load_from_json_file('add_item.json')
+    list = load_from_json_file(file)
 except FileNotFoundError:
     list = []
-
-list.append(sys.argv[1:])
-save_to_json_file(list, 'add_item.json')
+i = 1
+if 0 < len(argv) is not 1:
+    while i < len(argv):
+        list.append(argv[i])
+        i += 1
+        
+save_to_json_file(list, file)
