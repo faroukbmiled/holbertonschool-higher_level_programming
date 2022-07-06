3f27961437a16708dfff33e5e984c2b31d17ebbd
faroukbmiled <fcfaroukfc@gmail.com>
2022-07-06 12:50:47 +0100

    create and grant

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- script that creates the MySQL server user user_0d_1
+
+-- user_0d_1 password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
