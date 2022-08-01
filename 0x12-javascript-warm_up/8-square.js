4f2f1d4c91c442a98f1e027e5f0af838b1296ead
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-01 18:14:30 +0100

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (!size) {
+  console.log('Missing size');
+} else {
+    for (let i = 0; i < size; i++) {
+      let out = '';
+      for (let x = 0; x < size; x++) out += 'X';
+      console.log(out);
+    }
+  }
