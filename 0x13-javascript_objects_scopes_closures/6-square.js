98c4c24e954f19d96190b4e6388d303e23058dbb
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-02 20:51:26 +0100

    if undefined

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let out = '';
+      for (let i = 0; i < this.width; i++) out += 'C';
+      for (let x = 0; x < this.height; x++) {
+        console.log(out);
+      }
+    }
+  }
+};
