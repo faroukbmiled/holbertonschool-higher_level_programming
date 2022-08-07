0127a73f18a6830a19c281fc4125a88cd392f21e
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-07 17:58:49 +0100

    testing

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""2-my_filter_states"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    sqldb = MySQLdb.connect(host='localhost',
+                            port=3306,
+                            user=argv[1],
+                            password=argv[2],
+                            database=argv[3])
+    cursor = sqldb.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}'"
+                   .format(argv[4]))
+    row = cursor.fetchall()
+    for i in row:
+        print(i)
+    cursor.close()
+    sqldb.close()
