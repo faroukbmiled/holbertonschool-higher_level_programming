bcccb74d0ce715722bfdf163c67e1b0127c068e7
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-19 13:58:34 +0100

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter"""
+from requests import post
+from sys import argv
+
+if __name__ == "__main__":
+    reqpost = post(argv[1], {'email': argv[2]})
+    print(reqpost.text)
