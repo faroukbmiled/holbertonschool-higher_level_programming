c5806cd25c9850f1de674c31ddf5a39b442b7914
faroukbmiled <fcfaroukfc@gmail.com>
2022-08-19 12:07:57 +0100

    old py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         status = response.read()
         print('Body response:')
-        print(f'\t- type: {type(status)}')
-        print(f'\t- content: {status}')
-        print(f"\t- utf8 content: {status.decode('utf-8')}")
+        print('\t- type: {}'.format(type(status)))
+        print('\t- content: {}'.format(status))
+        print('\t- utf8 content: {}'.format(status.decode('utf-8')))
