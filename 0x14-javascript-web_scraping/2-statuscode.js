47a54211de75672c30da9a67b93f2d7a92babe99
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-13 16:22:02 +0100

    test

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('axios');
 const argv2 = process.argv[2];
 request(argv2, get);
 function get (notfound, argv2) { if (argv2) { console.log('code: ' + argv2.statusCode || notfound); } }
