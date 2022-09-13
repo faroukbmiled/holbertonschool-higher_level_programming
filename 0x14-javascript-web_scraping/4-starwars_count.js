7ed87d949f38ed6e9feb6dfc8b53f2422d7209d2
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-13 15:19:05 +0100

    test

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18/';
+const url = '18';
 request(process.argv[2], function (nf, none, resp) {
   if (nf); let i = 0;
   for (const response of JSON.parse(resp).results) {
