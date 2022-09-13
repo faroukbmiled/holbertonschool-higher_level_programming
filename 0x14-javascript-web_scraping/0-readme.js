066a51d54c6b0e3131707d52bf03ad015b887870
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-13 13:45:13 +0100

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const file = require('fs');
 file.readFile(process.argv[2], 'utf8', read);
-function read (notfound, found) {
-  console.log(notfound || found);
-}
+function read (notfound, found) { console.log(notfound || found); }
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,4 @@
 const file = require('fs');
 const argv3 = process.argv[3];
 file.writeFile(process.argv[2], argv3, write);
-function write (write) {
-  if (write) console.log(write);
-}
+function write (write) { if (write) console.log(write); }
