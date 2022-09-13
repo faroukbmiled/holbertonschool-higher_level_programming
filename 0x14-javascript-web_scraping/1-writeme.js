e0ca638674a230b9a475eeffcc467a2bea11c972
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-13 13:10:52 +0100

    fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const file = require('fs');
-argv3 = process.argv[3]
-file.writeFile(process.argv[2], argv3, write)
-  function write (write) {
+const argv3 = process.argv[3];
+file.writeFile(process.argv[2], argv3, write);
+function write (write) {
   if (write) console.log(write);
-};
-
+}
