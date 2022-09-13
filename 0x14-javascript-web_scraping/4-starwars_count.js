d2557d714152ea49b075fc870aadff04e9924886
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-13 15:19:52 +0100

    test

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = '18';
 request(process.argv[2], function (nf, none, resp) {
   if (nf); let i = 0;
   for (const response of JSON.parse(resp).results) {
     for (const char of response.characters) {
-      i += (char.includes(url));
+      i += (char.includes("18"));
     }
   }
   console.log(i);
